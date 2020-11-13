b723087748dd50570ffbf37e6aa87892f13f89f7
fengsy <fengsy@yiche99.com>
2020-11-13 16:39:57 +0800

    初始化封装集合

---
+++ my-package/src/components/MessageBox/MessageBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      弹框
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MessageBox"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ my-package/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    初始化封装类
+    <MessageBox></MessageBox>
+  </div>
+</template>
+
+<script>
+import MessageBox from '@/components/MessageBox/MessageBox'
+export default {
+  name: 'Home',
+  components: {
+    MessageBox
+  }
+}
+</script>
+<style>
+
+</style>
